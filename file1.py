692ab133721f10fce74f7ced39fc045c21a4a2ca
Sneha <snehakasturi2006@gmail.com>
2024-10-22 16:27:50 +0530

    Initial commit

--- file1.py
+++ file1.py
@@ -8,7 +8,7 @@ output_data = dataset[['calories_burned']]
 from sklearn.preprocessing import MinMaxScaler
 scaler=MinMaxScaler()
 input_data_scaled=scaler.fit_transform(input_data)
-print("Hello")
+
 
 from sklearn.model_selection import train_test_split
 input_data_train,input_data_test,output_data_train,output_data_test=train_test_split(input_data_scaled,output_data,test_size=0.2,random_state=42)
